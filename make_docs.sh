726000a65bb715c45fc002b380b9a7f5023796b8
Hauke Strasdat <strasdat@gmail.com>
2023-09-10 21:53:30 -0700

    docs: update due reflection farm-ng-core+sophus2 merge

--- make_docs.sh
+++ make_docs.sh
@@ -8,8 +8,6 @@ mkdir projects
 cd projects
 git clone https://github.com/jothepro/doxygen-awesome-css.git
 git clone https://github.com/farm-ng/farm-ng-core.git
-git clone -b sophus2 https://github.com/strasdat/Sophus.git
-git clone https://github.com/farm-ng/farm-ng-amiga.git
 cd ..
 
 doxygen doxyfile
